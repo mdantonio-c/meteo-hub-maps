a884330a93977284e79ef2cf700d612d6ad10bac
bchiavarini <b.chiavarini@cineca.it>
2021-06-25 18:03:08 +0200

    test MapSet endpoint

--- projects/maps/backend/tests/test_api_maps.py
+++ projects/maps/backend/tests/test_api_maps.py
@@ -1,19 +1,22 @@
-from faker import Faker
 from pathlib import Path
+
+from faker import Faker
 from flask.wrappers import Response
-from restapi.tests import API_URI, BaseTests, FlaskClient
 from maps.endpoints.config import (
     AREAS,
+    DEFAULT_PLATFORM,
     ENVS,
     FIELDS,
     LEVELS_PE,
     LEVELS_PR,
+    MEDIA_ROOT,
     PLATFORMS,
     RESOLUTIONS,
     RUNS,
-    MEDIA_ROOT
 )
+from restapi.tests import API_URI, BaseTests, FlaskClient
 from restapi.utilities.logs import log
+
 
 class TestApp(BaseTests):
     def test_api_maps(self, client: FlaskClient, faker: Faker) -> None:
@@ -25,46 +28,77 @@ class TestApp(BaseTests):
         res = RESOLUTIONS[0]
         area = AREAS[0]
         for i in FIELDS:
-            if i != "percentile" or i != "probability": # discard the iff fields
+            if i != "percentile" or i != "probability":  # discard the iff fields
                 field = i
                 break
-        platform = PLATFORMS[0]
+        platform = DEFAULT_PLATFORM
         env = ENVS[0]
         cosmo_map_dir = f"Magics-{run}-{res}.web"
-        cosmo_map_path = Path(MEDIA_ROOT,platform,env,cosmo_map_dir,area)
+        cosmo_map_path = Path(MEDIA_ROOT, platform, env, cosmo_map_dir, area)
         reftime = faker.pyint(1000000000, 9999999999)
         # create the base directory for the maps
         cosmo_map_path.mkdir(parents=True, exist_ok=True)
         # create the directory for the field
-        cosmo_field_dir = Path(cosmo_map_path,field)
+        cosmo_field_dir = Path(cosmo_map_path, field)
         cosmo_field_dir.mkdir(parents=True, exist_ok=True)
         # create a fake map file
         fcontent = faker.paragraph()
-        cosmo_mapfile_path = Path(cosmo_field_dir,f"{field}.{reftime}.{map_offset}.png")
+        cosmo_mapfile_path = Path(
+            cosmo_field_dir, f"{field}.{reftime}.{map_offset}.png"
+        )
         with open(cosmo_mapfile_path, "w") as f:
             f.write(fcontent)
 
         # get a map which is not ready yet
-        endpoint = API_URI + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 404
         not_ready_msg = self.get_content(r)
 
         # create a ready file
         cosmo_readyfile_path = Path(cosmo_map_path, f"{reftime}.READY")
-        open(cosmo_readyfile_path, 'a').close()
+        open(cosmo_readyfile_path, "a").close()
+
+        # TEST API FOR CHECK MAPS READINESS
+        for p in PLATFORMS:
+            if p != DEFAULT_PLATFORM:
+                no_avail_platform = p
+        ready_endpoint = (
+            API_URI
+            + f"/maps/ready?field={field}&run={run}&res={res}&area={area}&platform={no_avail_platform}&env={env}"
+        )
+        # # admin check for an unavailable platform
+        # admin_headers, _ = self.do_login(client, None, None)
+        # r = client.get(ready_endpoint,headers=admin_headers,)
+        # assert r.status_code == 500
+        # normal user check for an unaival platform
+        r = client.get(ready_endpoint)
+        assert r.status_code == 200
+        ready_res = self.get_content(r)
+        assert ready_res["reftime"] == str(reftime)
+        assert len(ready_res["offsets"]) == 1 and ready_res["offsets"][0] == map_offset
+        assert ready_res["platform"] == platform
 
         # get a map that does not exists
-        endpoint = API_URI + f"/maps/offset/{faker.pyint(1000, 9999)}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{faker.pyint(1000, 9999)}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 404
         not_found_msg = self.get_content(r)
 
-        #compare the different 404 messages
+        # compare the different 404 messages
         assert not_ready_msg != not_found_msg
 
         # get a map file
-        endpoint = API_URI + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 200
         retrieved_map_content = r.data.decode("utf-8")
@@ -83,14 +117,17 @@ class TestApp(BaseTests):
         perc_field_dir.mkdir(parents=True, exist_ok=True)
         # create a fake percentile map file
         fcontent = faker.paragraph()
-        perc_mapfile_path = Path(perc_field_dir, f"perc6.{reftime}.{map_offset}_{level_pe}.png")
-        print("*** ", perc_mapfile_path)
+        perc_mapfile_path = Path(
+            perc_field_dir, f"perc6.{reftime}.{map_offset}_{level_pe}.png"
+        )
         with open(perc_mapfile_path, "w") as f:
-            print("writing in ", perc_mapfile_path)
             f.write(fcontent)
 
         # get a map which is not ready yet
-        endpoint = API_URI + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pe={level_pe}"
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pe={level_pe}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 404
         iff_not_ready_msg = self.get_content(r)
@@ -99,10 +136,25 @@ class TestApp(BaseTests):
 
         # create a iff ready file
         iff_readyfile_path = Path(iff_map_path, f"{reftime}.READY")
-        open(iff_readyfile_path, 'a').close()
+        open(iff_readyfile_path, "a").close()
+
+        # test ready endpoint for percentile use case
+        perc_ready_endpoint = (
+            API_URI
+            + f"/maps/ready?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pe={level_pe}"
+        )
+        r = client.get(perc_ready_endpoint)
+        assert r.status_code == 200
+        ready_res = self.get_content(r)
+        assert ready_res["reftime"] == str(reftime)
+        assert len(ready_res["offsets"]) == 1 and ready_res["offsets"][0] == map_offset
+        assert ready_res["platform"] == platform
 
         # get a percentile map file
-        endpoint = API_URI + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pe={level_pe}"
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pe={level_pe}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 200
         retrieved_map_content = r.data.decode("utf-8")
@@ -119,12 +171,29 @@ class TestApp(BaseTests):
         perc_field_dir.mkdir(parents=True, exist_ok=True)
         # create a fake probability map file
         fcontent = faker.paragraph()
-        prob_mapfile_path = Path(perc_field_dir, f"prob6.{reftime}.{map_offset}_{level_pr}.png")
+        prob_mapfile_path = Path(
+            perc_field_dir, f"prob6.{reftime}.{map_offset}_{level_pr}.png"
+        )
         with open(prob_mapfile_path, "w") as f:
             f.write(fcontent)
 
-        # get a percentile map file
-        endpoint = API_URI + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pr={level_pr}"
+        # test ready endpoint for probability use case
+        prob_ready_endpoint = (
+            API_URI
+            + f"/maps/ready?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pr={level_pr}"
+        )
+        r = client.get(prob_ready_endpoint)
+        assert r.status_code == 200
+        ready_res = self.get_content(r)
+        assert ready_res["reftime"] == str(reftime)
+        assert len(ready_res["offsets"]) == 1 and ready_res["offsets"][0] == map_offset
+        assert ready_res["platform"] == platform
+
+        # get a probability map file
+        endpoint = (
+            API_URI
+            + f"/maps/offset/{map_offset}?field={field}&run={run}&res={res}&area={area}&platform={platform}&env={env}&level_pr={level_pr}"
+        )
         r = client.get(endpoint)
         assert r.status_code == 200
         retrieved_map_content = r.data.decode("utf-8")
@@ -132,8 +201,7 @@ class TestApp(BaseTests):
         # check if the retrieved file is the same created (and if it's complete)
         assert retrieved_map_content == fcontent
 
-
-        #delete all the files used for the tests
+        # delete all the files used for the tests
         Path.unlink(cosmo_readyfile_path)
         Path.unlink(cosmo_mapfile_path)
         Path.unlink(iff_readyfile_path)
