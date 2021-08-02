a6e3acda6f366fa2c6623b97a341f1a3309aada3
Mattia <m.dantonio81@gmail.com>
2021-08-02 08:41:38 +0200

    Fixed percentile probability platform from GALILEO to G100

--- projects/maps/backend/endpoints/maps.py
+++ projects/maps/backend/endpoints/maps.py
@@ -145,8 +145,7 @@ class MapSet(EndpointResource):
         log.debug(f"Retrieve map set for last run <{run}>")
 
         if field == "percentile" or field == "probability":
-            # force GALILEO as platform
-            platform = "GALILEO"
+            platform = "G100"
             log.warning("Forcing platform to {} because field is {}", platform, field)
 
         # if PLATFORM is not provided, set as default the first available
