5ff901738b4e5202478894567417b2d64a9e4e72
dcrisant <d.crisante@cineca.it>
2025-04-14 17:34:03 +0200

    if found multiple .READY files return the latest

--- projects/maps/backend/endpoints/config.py
+++ projects/maps/backend/endpoints/config.py
@@ -208,7 +208,8 @@ def get_geoserver_ready_file(base_path: Path, area: str) -> Optional[Path]:
     if not ready_files:
         return None
 
-    log.info(f".READY files found: {ready_files[0].__dict__}")
+    log.info(f".READY files found: {ready_files[0].name}")
+    log.info(f".READY files found: {ready_files[0].path}")
     try:
         res = ready_files.sort(key=lambda x: datetime.strptime(x.name.split(".")[0], "%Y%m%d%H"))[0]
     except Exception as e:
