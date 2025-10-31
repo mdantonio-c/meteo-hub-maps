8d406d66d0b2152076ef14e8412349074bb6e97f
dcrisant <d.crisante@cineca.it>
2025-10-31 12:55:40 +0100

    changed SLDs assigned to seasonal temperature means

--- projects/maps/backend/tasks/data_ready.py
+++ projects/maps/backend/tasks/data_ready.py
@@ -38,7 +38,8 @@ sld_dir_mapping = {
     "sf_6_12_24": ["snow6-snow", "snow12-snow", "snow24-snow"],
     "t2m": ["t2m-t2m"],
     "ws10m": ["wind-10u", "wind-10v", "wind-vmax_10m"],
-    "temp_anomaly": ["seasonal-ano-max-TM", "seasonal-ano-min-Tm", "seasonal-mean-TM", "seasonal-mean-Tm"],
+    "temp_anomaly": ["seasonal-ano-max-TM", "seasonal-ano-min-Tm"],
+    "temperature": ["seasonal-mean-TM", "seasonal-mean-Tm"],
     "precip_anomaly": ["seasonal-ano-P"],
     "precip_sum": ["seasonal-sum-P"],
 }
